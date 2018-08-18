74aedafd143cbeb6563c2c40e022b2db3c3a0285
Subramanian <clawsonfire97@gmail.com>
2018-08-19 02:30:12 +0530

    Add quotes_spider

---
+++ quotes_scraper/spiders/quotes_spider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quote-spider"
+
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+    
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").extract_first(),
+                'author': quote.css("small.author::text").extract_first(),
+                'tags': quote.css("div.tags a.tag::text").extract(),
+            }
+        next_page = response.css("li.next a::attr(href)").extract_first()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
